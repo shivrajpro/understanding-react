78889b9abf3c40e6e9bd2d241afbfd5e51e8df4b
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-05-07 00:46:28 +0530

    adding layout to the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Layout from "./layouts/Layout";
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupsPage from './pages/NewMeetups';
-import MainNavigation from './layouts/MainNavigation';
-
-import './App.css';
 
 function App() {
   return (
     <div>
 
-    <MainNavigation/>
+    {/* <MainNavigation/> */}
+    <Layout>
     <Switch>{/*if not used then we get nested pages */}
       <Route path='/' exact> {/* similar to pathMatch in Angular */}
         <AllMeetupsPage />
@@ -24,6 +24,7 @@ function App() {
         <FavoritesPage />
       </Route>
     </Switch>
+    </Layout>
     </div>
   );
 }
--- src/components/meetups/meetup-item/MeetupItem.js
+++ src/components/meetups/meetup-item/MeetupItem.js
@@ -1,22 +1,25 @@
+import Card from "../../../layouts/UI/card/Card";
 import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
     const item = props.meetup;
 
     return (
-        <li className={classes.item}>
+        <Card>
+            <li className={classes.item}>
             <div className={classes.image}>
-                <img className={classes} src={item.image} alt={item.title} />
-            </div>
-            <div className={classes.content}>
-                <h3> {item.title} </h3>
-                <address> {item.address} </address>
-                <p>{item.description}</p>
-            </div>
-            <div className={classes.actions}>
-                <button>To Favorites</button>
-            </div>
-        </li>
+                    <img className={classes} src={item.image} alt={item.title} />
+                </div>
+                <div className={classes.content}>
+                    <h3> {item.title} </h3>
+                    <address> {item.address} </address>
+                    <p>{item.description}</p>
+                </div>
+                <div className={classes.actions}>
+                    <button>To Favorites</button>
+                </div>
+            </li>
+        </Card>
     )
 }
 

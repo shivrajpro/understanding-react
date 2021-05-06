b3377fb1fe1bb4ebe844681d44fd6445dbd1421a
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-05-06 19:45:49 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo title="Learn JS" />
-      <Todo title="Learn Angular" />
-      <Todo title="Learn React" />
+      <h1>Hello</h1>
     </div>
   );
 }
--- src/components/Backdrop.js
+++
@@ -1,6 +0,0 @@
-function Backdrop(props) {
-    console.log('>> bd props', props);
-    return <div className='backdrop' onClick={props.onBackdropClick} />
-}
-
-export default Backdrop;

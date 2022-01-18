bb95ba2cdf5abe635549d423996d931f3ff014f0
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2022-01-19 01:06:30 +0530

    printing expense date in a format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,22 @@
 import "./ExpenseItem.css";
-function ExpenseItem(props){
-    const expenseDate = new Date();
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294.67;
-    // console.log("qq props",props);
-    return (
-        <div className="expense-item" >
-            <div> {props.date.toISOString()} </div>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price" >
-                    ${props.amount}
-                </div>
-            </div>
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
+  console.log("qq year", year);
+  return (
+    <div className="expense-item">
+        <div>
+            <div> {month} </div>
+            <div> {year} </div>
+            <div> {day} </div>
         </div>
-    )
+        <div className="expense-item__description"></div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

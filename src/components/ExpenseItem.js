0d7ed25e8f7425ac703394e3d34755cdd95b908c
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2022-01-19 00:46:42 +0530

    introduction to props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
 import "./ExpenseItem.css";
-function ExpenseItem(){
+function ExpenseItem(props){
+    const expenseDate = new Date();
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
+    // console.log("qq props",props);
     return (
         <div className="expense-item" >
-            <div>19th Jan 2022</div>
+            <div> {props.date.toISOString()} </div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price" >$294.67</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price" >
+                    ${props.amount}
+                </div>
             </div>
         </div>
     )

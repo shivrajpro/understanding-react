707d6afa794cc102df3b0a9e83f0b5e336959fec
Shivraj Hulbatte <shivraj.hulbatte@coditas.com>
2022-01-18 23:22:55 +0530

    react getting started

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Layout from "./layouts/Layout";
-import AllMeetupsPage from './pages/AllMeetups';
-import FavoritesPage from './pages/Favorites';
-import NewMeetupsPage from './pages/NewMeetups';
-
 function App() {
   return (
     <div>
-
-    {/* <MainNavigation/> */}
-    <Layout>
-    <Switch>{/*if not used then we get nested pages */}
-      <Route path='/' exact> {/* similar to pathMatch in Angular */}
-        <AllMeetupsPage />
-      </Route>
-
-      <Route path='/newmeetups'>
-        <NewMeetupsPage />
-      </Route>
-      
-      <Route path='/favorites' >
-        <FavoritesPage />
-      </Route>
-    </Switch>
-    </Layout>
+      <h2>Let's get started!</h2>
     </div>
   );
 }
